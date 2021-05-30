e8e0ea4ff2d46af705743495d15d7ee6dd49d3dc
Khushbu Bora <72873874+KhushbuBora@users.noreply.github.com>
2021-05-30 12:53:29 +0530

    Update CustomerHash.java

--- CustomerHash.java
+++ CustomerHash.java
@@ -1,4 +1,4 @@
-package miniProject;
+package medical_Store_Management;
 
 import java.util.HashMap;
 
@@ -7,7 +7,12 @@ public class CustomerHash {
 	HashMap<String, Customer> customerDatabase = new HashMap<>();
 	
 	void insert(Medical_avl  m){
-		Customer new_customer = m.customer_order();
+		
+		Customer new_customer = new Customer();
+		new_customer.accept();
+		
+		m.customer_order(new_customer);
+		
 		customerDatabase.put(new_customer.id, new_customer);
 	}
 }
