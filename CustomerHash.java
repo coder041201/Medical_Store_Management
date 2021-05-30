11173053fd106ff3259aedead4027253f01fe6e7
Khushbu Bora <72873874+KhushbuBora@users.noreply.github.com>
2021-05-30 23:26:58 +0530

    Update CustomerHash.java

--- CustomerHash.java
+++ CustomerHash.java
@@ -5,34 +5,33 @@ import java.util.Map;
 
 public class CustomerHash {
 
-	HashMap<String, Customer> customerDatabase = new HashMap<>();
+	static HashMap<String, Customer> customerDatabase = new HashMap<>();
 	
-	void insert(Customer new_customer){
+	static void insert(Customer new_customer){
 		
 		customerDatabase.put(new_customer.id, new_customer);
 		
 	}
 	
-	void  displayTotalSales(){
+	static void  displayTotalSales(){
 		
 		System.out.println("Total transactions till date");
 		int sales = 0;
 		Customer value;
 		//tagline
-		System.out.println("Customer id/tName/tBill (Rs.)");
+		System.out.println("Customer id\tName\tBill (Rs.)");
 		
-		for ( Map.Entry mapElement : customerDatabase.entrySet()) {
+		for ( @SuppressWarnings("rawtypes") Map.Entry mapElement : customerDatabase.entrySet()) {
+			
     		value = (Customer)mapElement.getValue();
+    		
 			sales+=value.bill;
-			System.out.println(value.id +"/t"+value.name+"/t"+ value.bill);
+			System.out.println(value.id +"\t"+value.name+"\t"+ value.bill);
         }
-		System.out.println("Total revenue generated:/t/t(Rs.)" + sales);
+		System.out.println("Total revenue generated:\t\t(Rs.)" + sales);
 	}
 	
-	/*customer create....n
-	 * ....
-	 * h.insert(n);
-	*/
+	
 	
 	
 }
