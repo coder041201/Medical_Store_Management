59709ede3fcf92c2c72c8c1e4a1280f9998a1e1a
Khushbu Bora <72873874+KhushbuBora@users.noreply.github.com>
2021-05-30 18:00:32 +0530

    Update CustomerHash.java

--- CustomerHash.java
+++ CustomerHash.java
@@ -1,18 +1,38 @@
 package medical_Store_Management;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerHash {
 
 	HashMap<String, Customer> customerDatabase = new HashMap<>();
 	
-	void insert(Medical_avl  m){
-		
-		Customer new_customer = new Customer();
-		new_customer.accept();
-		
-		m.customer_order(new_customer);
+	void insert(Customer new_customer){
 		
 		customerDatabase.put(new_customer.id, new_customer);
+		
 	}
+	
+	void  displayTotalSales(){
+		
+		System.out.println("Total transactions till date");
+		int sales = 0;
+		Customer value;
+		//tagline
+		System.out.println("Customer id/tName/tBill (Rs.)");
+		
+		for ( Map.Entry mapElement : customerDatabase.entrySet()) {
+    		value = (Customer)mapElement.getValue();
+			sales+=value.bill;
+			System.out.println(value.id +"/t"+value.name+"/t"+ value.bill);
+        }
+		System.out.println("Total revenue generated:/t/t(Rs.)" + sales);
+	}
+	
+	/*customer create....n
+	 * ....
+	 * h.insert(n);
+	*/
+	
+	
 }
