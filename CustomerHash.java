8ef82e84edb65bd81264ce0c6f85672de7eee7ba
Khushbu Bora <72873874+KhushbuBora@users.noreply.github.com>
2021-06-01 06:50:44 +0530

    Update CustomerHash.java

--- CustomerHash.java
+++ CustomerHash.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class CustomerHash {
 
-	static HashMap<String, Customer> customerDatabase = new HashMap<>();
-	
+	static HashMap<Integer, Customer> customerDatabase = new HashMap<>();
+	static int curr_id = 1234;
 	static void insert(Customer new_customer){
 		
 		customerDatabase.put(new_customer.id, new_customer);
@@ -19,16 +19,16 @@ public class CustomerHash {
 		int sales = 0;
 		Customer value;
 		//tagline
-		System.out.println("Customer id\tName\tBill (Rs.)");
+		System.out.println("Customer id\tName\t  Bill (Rs.)");
 		
 		for ( @SuppressWarnings("rawtypes") Map.Entry mapElement : customerDatabase.entrySet()) {
 			
     		value = (Customer)mapElement.getValue();
     		
 			sales+=value.bill;
-			System.out.println(value.id +"\t"+value.name+"\t"+ value.bill);
+			System.out.println(value.id +"\t\t"+value.name+"\t  "+ value.bill);
         }
-		System.out.println("Total revenue generated:\t\t(Rs.)" + sales);
+		System.out.println("Total revenue generated: (Rs.)" + sales);
 	}
 	
 	
